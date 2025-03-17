4357878b73a39af519a33f1ff8ccb37880993295
DavidOS <48949494+magodago@users.noreply.github.com>
2025-03-17 23:13:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -679,4 +679,27 @@ document.addEventListener("DOMContentLoaded", () => {
       projectionsOutput.textContent = projectionsData[randomIdx];
     });
   }
+
+  /* ---------------------------
+     15. Efectos de Scroll Animado
+  --------------------------- */
+  const scrollElements = document.querySelectorAll(".animate-on-scroll");
+  const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return (
+      elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+  };
+  const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+  };
+  const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+      if (elementInView(el, 1.25)) {
+        displayScrollElement(el);
+      }
+    });
+  };
+  window.addEventListener("scroll", handleScrollAnimation);
+  handleScrollAnimation();
 });
